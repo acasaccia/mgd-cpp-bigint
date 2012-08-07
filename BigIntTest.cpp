17eabd71719a3582e3052359d1c5f4c576596226
acasaccia <acasaccia@bfd5ccec-2ed3-e4be-69ae-99bb8b065f42>
2012-08-07 06:36:20 +0000

    Inizio definizione interfaccia e prima implementazione rappresentazione interna.

---
+++ BigIntTest.cpp
@@ -0,0 +1,30 @@
+/*! 
+ *  BigInt test
+ *  -
+ *  BigInt Homework - Advanced cpp programming
+ *  Master in Computer Game Development, Verona, Italy
+ *  -
+ *  \author Andrea Casaccia
+ *  \date 5 August 2012
+ */
+
+#include <ctime>
+#include <string>
+#include <vector>
+#include <iostream>
+#include "BigInt.h"
+
+int main(int argc, char **argv) {
+
+	std::clock_t begin = clock();
+
+	std::vector<std::string> args(argv, argv + argc);
+
+	BigInt* test = new BigInt(15);
+
+	clock_t end = clock();
+	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+	std::cout << "Done [Elapsed: " << elapsed_secs << "\"]" << std::endl;
+
+	return EXIT_SUCCESS;
+}
