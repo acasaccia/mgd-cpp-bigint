7a6c400b28fd767f6562ff36423ac038329330a6
acasaccia <acasaccia@bfd5ccec-2ed3-e4be-69ae-99bb8b065f42>
2012-09-17 07:50:43 +0000

    Exceptions.h missing in previous commit. Fixed wrong exception caught in operator>> body.

---
+++ trunk/Exceptions.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include <exception>
+
+class BadIntegerInitializationException : std::exception {
+	const char* what() const {
+		return "Can't initialize from negative integer.";
+	}
+};
+
+class BadStringInitializationException : std::exception {
+	const char* what() const {
+		return "Can't initialize from string. Allowed characters: [0-9].";
+	}
+};
+
+class InvalidSubtractionException : std::exception {
+	const char* what() const {
+		return "Tried to perform subtraction on two UnsignedBigInt which would have caused a negative value.";
+	}
+};
